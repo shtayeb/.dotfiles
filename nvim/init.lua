4e91e441b27e553d5c165da8dae287e1a3e33297
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-04-22 13:41:23 +0430

    no line number in nivm terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -163,6 +163,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 ----
 ---- Custom
 ----
+vim.api.nvim_command("autocmd TermOpen * startinsert")             -- starts in insert mode
+vim.api.nvim_command("autocmd TermOpen * setlocal nonumber")       -- no numbers
+vim.api.nvim_command("autocmd TermEnter * setlocal signcolumn=no")
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
