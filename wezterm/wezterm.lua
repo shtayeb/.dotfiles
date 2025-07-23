718b2af0b03fe8f82cfbf19e9452256991bd37c1
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-07-23 17:20:09 +0900

    updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,10 +3,12 @@ local Config = require('config')
 require('events.tab-title').setup()
 -- require('events.new-tab-button').setup()
 
-return Config:init()
+local config = Config:init()
    :append(require('config.appearance'))
    -- :append(require('config.bindings'))
    :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
-   :append(require('config.launch')).options
+   :append(require('config.launch'))
+
+return config.options
